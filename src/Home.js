8acac35992a79be1de3fa5b6a635dec01ecc0d12
gajeranehal <gajeranehal9@gmail.com>
2024-09-11 23:30:31 +0530

    firstupload

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Header from "./Homepage/Header"
+import Slider from "./Homepage/Slider"
+
+function Home() {
+    return(
+        <>
+            <h1>Hello Welcome To My Kingdom</h1>
+            <Header/>
+            <Slider/>
+        </>
+    )
+}
+
+export default Home
---
+++ src/Homepage/Slider.js
@@ -0,0 +1,9 @@
+function Slider() {
+    return(
+        <>
+            <h1>Slider Component</h1>
+        </>
+    )
+}
+
+export default Slider
